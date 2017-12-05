564fbf368fbd75f5385d71b2c002a62002c81a32
patriciadhl <patricia.dione@gmail.com>
2017-12-05 12:27:15 -0600

    Actualizando

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
+function nameList(){
+  //creando boton Guardar nombre de lista
+  var btn = document.createElement("button");
+  var textBtn = document.createTextNode("Guardar");
+  btn.setAttribute("id","boton1");
+  btn.setAttribute("type","button");
+  btn.appendChild(textBtn);
+  btn.addEventListener("click", createList);
+  document.getElementById("btn").appendChild(btn);
+  var boton=document.getElementById("boton1");
+  boton.className=("boton");
+  //Agregando clase nueva a formulario
+  var contForm=document.getElementById("form1");
+  contForm.className=("formList");
 
+}
+
+function createList(){
+  console.log("entrando");
+  var z=document.getElementById("form2");
+  z.className = "d-inlineblock";
+}
